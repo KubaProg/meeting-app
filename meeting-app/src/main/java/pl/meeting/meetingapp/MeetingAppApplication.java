fe31107c77219afbfbdd7235f6dee8162fd5a1aa
Jakub_Opielka <jakub.opielka@q-perior.com>
2023-08-28 09:53:46 +0200

    Added db schema file + renamed main app class

--- meeting-app/src/main/java/pl/meeting/meetingapp/MeetingAppApplication.java
+++ meeting-app/src/main/java/pl/meeting/meetingapp/MeetingAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorkshopManagementApplication {
+public class MeetingAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WorkshopManagementApplication.class, args);
+        SpringApplication.run(MeetingAppApplication.class, args);
     }
 
 }
