5c0c9c358b1a655d8c6bdadc501f8a978a4c5058
Dominik Szczepanik <dominik16333@gmail.com>
2023-09-13 02:53:38 +0200

    h2 database for tests

--- meeting-app/src/test/java/pl/meeting/meetingapp/controller/UserControllerTest.java
+++ meeting-app/src/test/java/pl/meeting/meetingapp/controller/UserControllerTest.java
@@ -1,63 +1,20 @@
 package pl.meeting.meetingapp.controller;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import pl.meeting.meetingapp.config.JwtService;
-import pl.meeting.meetingapp.entity.User;
-import pl.meeting.meetingapp.mapper.UserMapper;
-import pl.meeting.meetingapp.repository.ProfileRepository;
-import pl.meeting.meetingapp.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import pl.meeting.meetingapp.MeetingAppApplication;
 import pl.meeting.meetingapp.repository.UserRepository;
-import pl.meeting.meetingapp.service.UserService;
 
-import java.util.Optional;
-
-import static org.mockito.Mockito.when;
-
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MeetingAppApplication.class)
 class UserControllerTest {
 
-    @Mock
-    private UserRepository userRepository;
+    @Autowired
+    UserRepository userRepository;
 
-    @Mock
-    private RoleRepository roleRepository;
-
-    @Mock
-    private PasswordEncoder passwordEncoder;
-
-    @Mock
-    private JwtService jwtService;
-
-    @Mock
-    private AuthenticationManager authenticationManager;
-
-    @Mock
-    private ProfileRepository profileRepository;
-
-
-//    private final ProfileMapper profileMapper = new ProfileMapperImpl();
-//
-//
-//    private final UserMapper userMapper = new UserMapperImpl();
-
-    @Mock
-    private UserService userService;
-
-    @Mock
-    private UserMapper userMapper;
-
-    @InjectMocks
-    private UserController userController;
 
     @Test
     void addUser() {
@@ -75,55 +32,10 @@ class UserControllerTest {
     void putUserById() {
     }
 
-    @BeforeEach
-    public void setUp() {
-        // initializing mocks
-        MockitoAnnotations.initMocks(this);
+    @Test
+    void getAllUsers() {
 
-        // setting up userRepository
-        User user = new User();
-        when(userRepository.findByUsername("testuser")).thenReturn(Optional.of(user));
-
-
-        // setting up authenticationManager
-        Authentication authentication = new UsernamePasswordAuthenticationToken("testuser", "testpassword");
-        when(authenticationManager.authenticate(authentication)).thenReturn(authentication);
-
-        // setting up jwtService
-        when(jwtService.generateToken(user)).thenReturn("mockedToken");
-
-
-//        // setting up userService
-//        UserRegisteredModelApi userRegisteredModelApi = userMapper.mapToUserRegisteredModelApi(user);
-//        userRegisteredModelApi.setJwtToken("mockedToken");
-//
-//        when(userService.logInByCredentials(userMapper.mapToUserLoginModelApi(user))).thenReturn(
-//                userRegisteredModelApi
-//        );
     }
-
-//    @Test
-//    void logInByCredentials_returnsToken() {
-//        User testuser = userRepository.findByUsername("testuser").get();
-//        String token = userController.logInByCredentials(userMapper.mapToUserLoginModelApi(testuser)).getBody().getJwtToken();
-//        assertEquals("mockedToken", token);
-//    }
-
-
-
-//    @Test
-//    void getAllUsers() {
-//
-//        UserModelApi user = new UserModelApi();
-//
-//        List<UserModelApi> users = List.of(user);
-//        when(userService.getAllUsers()).thenReturn(users);
-//
-//        List<UserModelApi> allUsers = userController.getAllUsers().getBody();
-//
-//        assertEquals(1, allUsers.size());
-//
-//    }
 
     @Test
     void getUserProfile() {
@@ -131,15 +43,6 @@ class UserControllerTest {
 
     @Test
     void deleteUserById() {
-//        UserController userController = mock(UserController.class);
-
-
-//        userController.deleteProfileByUserId(1L);
-
-//        ProfileModelApi userProfile = userController.getUserProfile(1L).getBody();
-
-
-//        assertEquals(2,userProfile);
 
     }
 }
