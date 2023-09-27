8feb6e804da18068f75241557fb74cb998ba6a8e
Dominik Szczepanik <dominik16333@gmail.com>
2023-09-27 18:23:00 +0200

    acces control allow origin added to cors config

--- meeting-app/src/main/java/pl/meeting/meetingapp/config/CorsFilter.java
+++ meeting-app/src/main/java/pl/meeting/meetingapp/config/CorsFilter.java
@@ -18,7 +18,7 @@ public class CorsFilter extends OncePerRequestFilter {
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
+        response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token, access-control-allow-origin");
         response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
         if ("OPTIONS".equals(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
